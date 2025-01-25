9539a8fc61505144e33863f2ddff556968ffe3c6
Moamen <moemen0101@gmail.com>
2025-01-25 11:01:50 +0200

    no subscribe for no chrome

--- alehyaa/app.js
+++ alehyaa/app.js
@@ -23,12 +23,24 @@ window.addEventListener("load", () => {
             QD("div#update-notice").style.display = 'block';
     }, 5000)
 })
-
+var browser = (function (agent) {
+    switch (true) {
+        case agent.indexOf("edge") > -1: return "edge";
+        case agent.indexOf("edg/") > -1: return "edge-chromium"; // Match also / to avoid matching for the older Edge
+        case agent.indexOf("opr") > -1 && !!window.opr: return "opera";
+        case agent.indexOf("chrome") > -1 && !!navigator.brave: return "brave";
+        case agent.indexOf("chrome") > -1 && !!window.chrome: return "chrome";
+        case agent.indexOf("trident") > -1: return "ie";
+        case agent.indexOf("firefox") > -1: return "firefox";
+        case agent.indexOf("safari") > -1: return "safari";
+        default: return "other";
+    }
+})(window.navigator.userAgent.toLowerCase());
 const supportsInstallPrompt = 'onbeforeinstallprompt' in window;
 //let supportsInstallPrompt = false;
 function updateSubscribeStatus(){
-    var isChromium = !!window.chrome;
-    if(!isChromium){
+    //var isChromium = !!window.chrome;
+    if(browser != "chrome"){
         oneChildVisible(QD("#subscribe"), QD("#no-chrome"));
         return;
     }
